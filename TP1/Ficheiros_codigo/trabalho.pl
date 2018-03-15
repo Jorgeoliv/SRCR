9dbfbca6d246ef6565b675e2e9e97ba3a3a4421f
José Pedro Ferreira <zepedroferreira11@gmail.com>
2018-03-15 22:29:33 +0000

    Replace trabalho.pl -> só testei o adicionarPrestador, adicionarCuidado e removerPrestador
    Necessário depois por tbm tudo direito em termos de manipulação de conhecimento

--- TP1/Ficheiros_codigo/trabalho.pl
+++ TP1/Ficheiros_codigo/trabalho.pl
@@ -15,36 +15,40 @@
 % SICStus PROLOG: definicoes iniciais
 
 :- op( 900,xfy,'::' ).
+:- dynamic utente/4.
+:- dynamic prestador/4.
 :- dynamic cuidado/6.
+:- dynamic instituicao/4.
 
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado utente: IdUt, Nome, Idade, Morada -> {V,F}
 
-utente(1,ana,14,rua).
-utente(2,luis,13,tres).
-utente(3,joao,13,lll).
+utente( 1,ana,14,morada( 'Rua do Louro','Caldas das Taipas','Guimaraes' ) ).
+utente( 2,luis,13,morada( 'Rua do Azevinho','Braga','Braga' ) ).
+utente( 3,joao,13,morada( 'Rua do Rual','São João de Ponte','Guimaraes' ) ).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado prestador: IdPrest, Nome, Especialidade, Instituição -> {V,F}
 
-prestador(1,jose,nano,chhc).
-prestador(2,joao,filo,rethc).
-prestador(3,jorge,tilo,chhc).
+prestador( 1,jose,'ortopedia',['Hospital de Braga'] ).
+prestador( 2,joao,'urologia',['Hospital de Guimaraes'] ).
+prestador( 3,jorge,'neurologia',['Hospital de Guimaraes'] ).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado cuidado: Data, IdUt, IdPrest, Descrição, Custo, Instituição -> {V,F}
 
-cuidado(11,1,1,err,23,chhc).
-cuidado(11,2,3,err,23,chhc).
-cuidado(12,3,3,err,23,chhc).
-cuidado(14,2,1,err,23,chhc).
-cuidado(15,3,3,trrr,34,chhc).
+cuidado( data( 2018,3,14,20 ),1,1,'curativo',23,'Hospital de Braga'  ).
+cuidado( data( 2018,3,10,20 ),2,3,'rotina',23,'Hospital de Guimaraes' ).
+cuidado( data( 2018,3,10,20 ),3,3,'rotina',23,'Hospital de Guimaraes' ).
+cuidado( data( 2018,2,10,20 ),2,1,'emergência',23,'Hospital de Braga' ).
+cuidado( data( 2018,3,15,20 ),3,3,'curativo',34,'Hospital de Guimaraes' ).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado instituição: Nome, Cidade, Tipo, ListaEspecialidade -> {V,F}
 
-instituição(chhc, tonios, nada, [nano, filo, tilo]).
+instituicao( 'Hospital de Braga','Braga','Hospital',['ortopedia'] ).
+instituicao( 'Hospital de Guimaraes','Guimaraes','Hospital',['urologia','neurologia'] ).
 
 
 %------------------------INVARIANTES-----------------------
@@ -56,11 +60,11 @@ instituição(chhc, tonios, nada, [nano, filo, tilo]).
 
 % não se pode remover utentes e prestadores para os quais existam cuidados
 
-% ----------------------------------Data do cuidado é válida---------------------------------------
+%----------------------------------Data do cuidado é válida---------------------------------------
 
-+cuidado(Data,U,P,Descricao,C,I) :: (dataValida(Data)).
++cuidado( Data,U,P,Descricao,C,I ) :: (dataValida(Data)).
 
-% ----------------------------------Não pode haver mais do que 3 cuidados à mesma hora tanto para o utente como o profissional-----------------
+%----------------------------------Não pode haver mais do que 3 cuidados à mesma hora tanto para o utente como o profissional-----------------
 
 +cuidado(D,U,P,Des,C,I) :: (solucoes((D,P),cuidado(D,Ut,P,Descr,Cus,Ins),L),
 							comprimento(L,X),
@@ -77,7 +81,7 @@ validaIdade( A ) :- R is A+1,
 					natural(R).
 
 
-dataValida( A,M,D,H ) :- 	natural(A),
+dataValida( data(A,M,D,H) ) :- 	natural(A),
 							natural(M),
 							natural(D),
 							natural(H),
@@ -106,48 +110,48 @@ diasValidos( A,2,D ) :- R is A mod 4,
 
 %-------- Não podem haver ids repetidos -------------
 
-+utente( IdU,_,_,_ ) :: solucoes( IdU,utente( IdU,_,_,_ ),L ),
++utente( IdU,_,_,_ ) :: (solucoes( IdU,utente( IdU,_,_,_ ),L ),
 						comprimento( L,X ),
-						X <= 1.
+						X =< 1).
 
-+prestador( IdP,_,_,_ ) :: solucoes( IdP,prestador( IdP,_,_,_ ),L ),
++prestador( IdP,_,_,_ ) :: (solucoes( IdP,prestador( IdP,_,_,_ ),L ),
 						comprimento( L,X ),
-						X <= 1.
+						X =< 1).
 
 %--------------- Não se podem adicionar cuidados para os quais não existam utentes/prestadores -------------------
 
-+cuidado( _,IdU,_,_,_,_ ) :: solucoes( IdU,utente( IdU,_,_,_ ),L ),
++cuidado( _,IdU,_,_,_,_ ) :: (solucoes( IdU,utente( IdU,_,_,_ ),L ),
 						comprimento( L,X ),
-						X == 1.
+						X == 1).
 
-+cuidado( _,_,IdP,_,_,_ ) :: solucoes( IdP,prestador( IdP,_,_,_ ),L ),
++cuidado( _,_,IdP,_,_,_ ) :: (solucoes( IdP,prestador( IdP,_,_,_ ),L ),
 						comprimento( L,X ),
-						X == 1.
+						X == 1).
 
 %--------------- Não se pode adicionar um cuidado prestado numa instituição da qual o prestador não faça parte ---------------
 
-+cuidado( _,_,IdP,_,_,_ ) :: solucoes( LI,( prestador( IdP,_,_,_ ),pertence(I,LI) ),L ),
++cuidado( _,_,IdP,_,_,I ) :: (solucoes( LI,( prestador( IdP,_,_,LI ),pertence( I,LI ) ),L ),
 						comprimento( L,X ),
-						X == 1.
+						X == 1).
 
 %--------------- Não se pode adicionar um cuidado cujo custo seja negativo ------------------------
 
-+cuidado( _,_,_,_,C,_ ) :- C >= 0.
++cuidado( _,_,_,_,C,_ ) :: (C >= 0).
 
 %--------------- Não se pode remover um utente para o qual existam cuidados relativos ---------------
 
--utente( IdU,_,_,_ ) :: solucoes( D,cuidado(D,IdU,_,_,_,_ ),L ),
+-utente( IdU,_,_,_ ) :: (solucoes( D,cuidado(D,IdU,_,_,_,_ ),L ),
 						comprimento( L,X ),
-						X == 0.
+						X == 0).
 
 
--prestador( IdP,_,_,_ ) :: solucoes( D,cuidado(D,_,IdP,_,_,_ ),L ),
+-prestador( IdP,_,_,_ ) :: (solucoes( D,cuidado(D,_,IdP,_,_,_ ),L ),
 						comprimento( L,X ),
-						X == 0.
+						X == 0).
 
 %--------------- Garantir que o prestador tem a especialidade que pertença à lista de especialidades da instituição a que pertença -----
 
-+prestador( IDp,_,Esp,LInst) :: validaEspecialidadeInstituicao( Esp,LInst ).
++prestador( IDp,_,Esp,LInst ) :: (validaEspecialidadeInstituicao( Esp,LInst )).
 
 validaEspecialidadeInstituicao( Esp,[] ).
 validaEspecialidadeInstituicao( Esp,[C|L] ) :- instituicao( C,_,_,E ),
@@ -161,14 +165,14 @@ validaEspecialidadeInstituicao( Esp,[C|L] ) :- instituicao( C,_,_,E ),
 %---------------- Adicionar utente ------------------------------------
 % adicionarUtente: Id,Nome,Idade,Morada -> {V,F}
 
-adicionarUtente( IdUtente,Nome,Idade,morada(R,L,C) ) :-
-											evolucao( utente( IdUtente,Nome,Idade,Morada( R,L,C ) ) ).
+adicionarUtente( IdUtente,Nome,Idade,Morada ) :-
+											evolucao( utente( IdUtente,Nome,Idade,Morada ) ).
 
 
 %---------------- Adicionar prestador ------------------------------------
 % adicionarPrestador: Id,Nome,Especialidade,ListaInstituicao -> {V,F}
 
-adicionarPrestador( IdPrestador,Nome,Espacialidade,ListaI ) :-
+adicionarPrestador( IdPrestador,Nome,Especialidade,ListaI ) :-
 											evolucao( prestador( IdPrestador,Nome,Especialidade,ListaI ) ).
 
 %---------------- Adicionar cuidado ------------------------------------
@@ -220,20 +224,102 @@ utenteMorada( localidade,M,R ) :- solucoes( utente( _,_,_,morada( M,_,_ ) ),uten
 utenteMorada( cidade,M,R ) :- solucoes( utente( _,_,_,morada( M,_,_ ) ),utente( _,_,_,morada( _,_,M ) ),R ).
 
 
-%----------------------------------------- Identificar prestadores a que um utente recorreu
-% prestadoresRecorridosUtente: IdUtente,R -> {V,F}
-prestadoresRecorridosUtente( IdU,R ) :- solucoes( ( IdP,N ),( cuidado( _,IdU,IdP,_,_ ),prestador(IdP,N,_,_) ),L ),
-											multiConjunto( L,R ).
-
 utentePorIdade( =,I,X ) :- solucoes( (Id,N),utente(Id,N,I,M),X ).
 utentePorIdade( <,I,X ) :- solucoes( (Id,N),(utente(Id,N,Idade,M), Idade < I),X ).
 utentePorIdade( >,I,X ) :- solucoes( (Id,N),(utente(Id,N,Idade,M), Idade > I),X ).
 utentePorIdade( =<,I,X ) :- solucoes( (Id,N),(utente(Id,N,Idade,M), Idade =< I),X ).
 utentePorIdade( >=,I,X ) :- solucoes( (Id,N),(utente(Id,N,Idade,M), Idade >= I),X ).
 
-instituicoesPrestCuidados( R ) :- solucoes( I,(cuidado(D,U,P,DE,C),prestador(P,N,E,I)),Aux ), multiConjunto(Aux,R).
+%----------------------------------------- Identificar prestadores a que um utente recorreu
+% prestadoresRecorridosUtente: IdUtente,R -> {V,F}
+prestadoresRecorridosUtente( IdU,R ) :- solucoes( ( IdP,N ),( cuidado( _,IdU,IdP,_,_ ),prestador(IdP,N,_,_) ),L ),
+											multiConjunto( L,R ).
+
+
+instituicoesPrestadoresCuidados( R ) :- solucoes( I,(cuidado(D,U,P,DE,C),prestador(P,N,E,I)),Aux ), multiConjunto(Aux,R).
 
 utentesPrestador( P,R ) :- solucoes( (U,N),(cuidado(D,U,P,DE,C),utente(U,N,I,M)),Aux ),  multiConjunto(Aux,R).
+
+
+%-------------------------- Ponto 5 -------------------------------------------------
+
+%% cuidados de saude prestados
+
+%%instituicao
+cuidadosSaudeInstituicao(Inst, R) :-
+	solucoes((D, IDU, IDP, Desc, Cus), cuidado(D, IDU, IDP, Desc, Cus, Inst), R).
+
+%%cidade
+cuidadosSaudeCidade(Cid, R) :-
+	solucoes((D, IDU, IDP, Desc, Cus, I), (utente(IDU, _, _, morada(_, Cid, _)) ,  cuidado(D, IDU, IDP, Desc, Cus, I)), R).
+
+%%Data
+
+comparaData(igual,data(A1, M1, D1, H1), data(A2, M2, D2, H2), data(A1, M1, D1, H1)) :-
+	A1 == A2,
+	M1 == M2,
+	D1 == D2,
+	H1 == H2.
+comparaData(maior,data(A1, M1, D1, H1), data(A2, M2, D2, H2), data(A1, M1, D1, H1)) :-
+	A1 > A2.
+comparaData(maior,data(A1, M1, D1, H1), data(A2, M2, D2, H2), data(A1, M1, D1, H1)) :-
+	A1 == A2,
+	M1 > M2.
+comparaData(maior,data(A1, M1, D1, H1), data(A2, M2, D2, H2), data(A1, M1, D1, H1)) :-
+	A1 == A2,
+	M1 == M2,
+	D1 > D2.
+comparaData(maior,data(A1, M1, D1, H1), data(A2, M2, D2, H2), data(A1, M1, D1, H1)) :-
+	A1 == A2,
+	M1 == M2,
+	D1 == D2,
+	H1 > H2.
+comparaData(menor,data(A1, M1, D1, H1), data(A2, M2, D2, H2), data(A1, M1, D1, H1)) :-
+	A1 < A2.
+comparaData(menor,data(A1, M1, D1, H1), data(A2, M2, D2, H2), data(A1, M1, D1, H1)) :-
+	A1 == A2,
+	M1 < M2.
+comparaData(menor,data(A1, M1, D1, H1), data(A2, M2, D2, H2), data(A1, M1, D1, H1)) :-
+	A1 == A2,
+	M1 == M2,
+	D1 < D2.
+comparaData(menor,data(A1, M1, D1, H1), data(A2, M2, D2, H2), data(A1, M1, D1, H1)) :-
+	A1 == A2,
+	M1 == M2,
+	D1 == D2,
+	H1 < H2.
+
+
+cuidadosSaudeData(igual, Data, R) :-
+	solucoes((D, IDU, IDP, Desc, Cus, Inst), (cuidado(D, IDU, IDP, Desc, Cus, Inst) , (comparaData(igual,D, Data, D))), R).
+cuidadosSaudeData(maior, Data, R) :-
+	solucoes((D, IDU, IDP, Desc, Cus, Inst), (cuidado(D, IDU, IDP, Desc, Cus, Inst) , (comparaData(maior,D, Data, D))), R).
+cuidadosSaudeData(menor, Data, R) :-
+	solucoes((D, IDU, IDP, Desc, Cus, Inst), (cuidado(D, IDU, IDP, Desc, Cus, Inst) , (comparaData(menor,D, Data, D))), R).
+cuidadosSaudeData(entre, Data1, Data2, R) :-
+	solucoes((D, IDU, IDP, Desc, Cus, Inst), (cuidado(D, IDU, IDP, Desc, Cus, Inst) , (comparaData(maior,D, Data1, D)),(comparaData(menor,D, Data2, D))), R).
+
+
+
+% -------------------------------- Ponto 6 -> Identificar os utentes de um prestador/especialidade/instituição ---------------------------------
+
+utentesDoPrestador( IdP,R ) :- solucoes( ( IdU,N ), ( cuidado(_,IdU,IdP,_,_,_), utente(IdU,N,_,_) ), L ),
+                               multiConjunto( L,R ).
+
+utentesDaEspecialidade( Esp,R ) :- solucoes( ( IdU,N ), ( prestador(IdP,_,Esp,_), cuidado(_,IdU,IdP,_,_,_), utente(IdU,N,_,_) ), L ),
+                                   multiConjunto( L,R ).
+
+utentesDaInstituicao( Inst,R ) :- solucoes( ( IdU,N ), ( cuidado(_,IdU,_,_,_,I), utente(IdU,N,_,_) ), L ),
+                                  multiConjunto( L,R ).											
+
+
+%------------------------------ Ponto 7 ---------------------------------------------------------
+
+cuidadosUtente(IDU,R) :- solucoes((D,IDU,PREST,DESC,CUSTO) , (cuidado(D,IDU,PREST,DESC,CUSTO)),R).
+
+cuidadosInstituicao(Inst,R) :- solucoes(  (D,IDU,PREST,DESC,CUSTO)  , ( prestador(PREST,_,_,Inst) , cuidado(D,IDU,PREST,DESC,CUSTO)), R).
+
+cuidadosPrestador(Prest,R) :- solucoes( (D,IDU,PREST,DESC,CUSTO)  , cuidado((D,IDU,Prest,DESC,CUSTO))  , R) .
 
 
 
@@ -305,4 +391,44 @@ take( [C|L],N,[C|R] ) :-
 	take( L,M,R ).
 
 
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado pertence: Elemento, Lista -> {V,F}
+pertence( X,[X|L] ).
+pertence( X,[C|L] ) :- pertence( X,L ).
 
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado comprimento: Lista, Resultado -> {V,F}
+comprimento( [],0 ).
+comprimento( [C|L],R ) :- comprimento( L,A ),
+					R is A+1.
+
+
+
+%---------------------------- Predicados de Evolucao -------------------
+
+evolucao( Termo ) :- solucoes( Inv,+Termo::Inv,S ),
+					inserir( Termo ),
+					testar( S ).
+
+inserir( P ) :- assert( P ).
+inserir( P ) :- retract( P ), !, fail.
+
+
+
+inevolucao( Termo ) :- solucoes(Inv,-Termo::Inv,S),
+					remover(Termo),
+					testar(S).
+
+
+remover( P ) :- retract( P ).
+remover( P ) :- assert( P ), !, fail.
+
+testar( [] ).
+testar( [X|R] ) :- X,
+				testar( R ).
+
+%-------------------------- Predicado não ----------------
+%-------- nao: Termo -> {V,F}
+
+nao( T ) :- T, !, fail.
+nao( T ). 
